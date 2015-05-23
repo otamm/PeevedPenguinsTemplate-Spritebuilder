3550f9b5c96ccc125635f7efb7f7dfc67b3af7b2
otamm <oivatom@gmail.com>
2015-05-23 18:15:38 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -193,6 +193,13 @@
     [seal removeFromParent];
 }
 
+- (void)startBlinkAndJump
+{
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
 
 - (void)retry {
     // reload the level
--- PeevedPenguins.spritebuilder/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -18,12 +18,5 @@
     [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
 }
 
-- (void)startBlinkAndJump
-{
-    // the animation manager of each node is stored in the 'animationManager' property
-    CCAnimationManager* animationManager = self.animationManager;
-    // timelines can be referenced and run by name
-    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
-}
 
 @end
